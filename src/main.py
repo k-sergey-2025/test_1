2a9a95c7aad4ee580e96b07b386f5cf73b8b7cf0
serzh <serzhcool@yandex.ru>
2025-03-16 15:10:43 +0300

    item user

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-app = FastAPI()
+from src.dependencies import get_token_header, get_query_token
+from User.router import router as user_router
+from item.items import router as item_router
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+app.include_router(user_router)
+app.include_router(item_router)
+
+
 
 @app.get("/")
-async def get_items():
-    return {"items": "hello"}
+async def root():
+    return {"message": "Hello Bigger Applications!"}
 
-@app.get("/item")
-async def get_item():
-    return {"item": "hello"}
+
+
+# @app.get("/item")
+# async def get_item():
+#     return {"item": "hello"}
 
